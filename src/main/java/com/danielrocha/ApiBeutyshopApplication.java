7873ea9fa24b94e4ac37fa769f5452c7fe36f0e7
Daniel Rocha <danielsantosr.rj@gmail.com>
2017-05-05 16:29:27 -0300

    start

---
+++ src/main/java/com/danielrocha/ApiBeutyshopApplication.java
@@ -0,0 +1,12 @@
+package com.danielrocha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBeutyshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBeutyshopApplication.class, args);
+	}
+}
