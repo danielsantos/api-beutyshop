4111d3ca1fbad5d6d8dd3595e56c376cf452eab0
Daniel Rocha <danielsantosr.rj@gmail.com>
2017-05-09 11:41:00 -0300

    Rest Controller Cliente

---
+++ src/main/java/com/danielrocha/controller/ClienteRestController.java
@@ -0,0 +1,45 @@
+package com.danielrocha.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.danielrocha.model.Cliente;
+import com.danielrocha.service.ClienteService;
+
+@RestController
+public class ClienteRestController {
+
+	@Autowired
+    private ClienteService service;
+
+
+    @RequestMapping("/clientes")
+    public Iterable<Cliente> listarTodos() {
+        return service.findAll();
+    }
+
+    @RequestMapping("/clientes/{id}")
+    public Cliente listarPorId(@PathVariable String id) {
+        return service.findOne(Long.parseLong(id));
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value="/clientes")
+    public void inserir(@RequestBody Cliente cliente) {
+        service.save(cliente);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value="/clientes/{id}")
+    public void atualizar(@RequestBody Cliente cliente, @PathVariable String id) {
+        service.save(cliente);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value="/clientes/{id}")
+    public void excluir(@PathVariable String id) {
+        service.delete(Long.parseLong(id));
+    }
+	
+}
