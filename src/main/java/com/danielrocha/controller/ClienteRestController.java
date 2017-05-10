1be0e7a5a6f72da9576ad0a08381248868345845
Daniel Rocha <danielsantosr.rj@gmail.com>
2017-05-10 11:39:37 -0300

    desenv

--- src/main/java/com/danielrocha/controller/ClienteRestController.java
+++ src/main/java/com/danielrocha/controller/ClienteRestController.java
@@ -2,6 +2,7 @@ package com.danielrocha.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,7 +28,7 @@ public class ClienteRestController {
         return service.findOne(Long.parseLong(id));
     }
 
-    @RequestMapping(method = RequestMethod.POST, value="/clientes")
+    @PostMapping(value="/clientes")
     public void inserir(@RequestBody Cliente cliente) {
         service.save(cliente);
     }
